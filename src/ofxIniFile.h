f17aaee74617978288938177aaaff0be39d357fc
roxlu <diederick@apollomedia.nl>
2010-08-15 17:24:39 +0200

    Updating some tinie fixes

--- src/ofxIniFile.h
+++ src/ofxIniFile.h
@@ -7,18 +7,18 @@ extern "C" {
 }
 
 /**
- * Simple wrapper for a c-style minimal ini setting parser. 
+ * Simple wrapper for a c-style minimal ini setting parser.
  */
 class ofxIniFile {
 public:
 	ofxIniFile(char* sFile);
 	~ofxIniFile();
-	
+
 	int	get(const char* sKey, int defaultValue);
 	double get(const char* sKey, double defaultValue);
 	char* get(const char* sKey, char* sDefaultValue);
 	bool get(const char* sKey, bool defaultValue);
-	
+
 private:
 	const char* ini_file;
 	dictionary*  ini;
