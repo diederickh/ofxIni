31d344c2f05805e62342a57588894bff41c3ac53
unknown <multimedia@.(none)>
2010-09-25 14:09:55 +0200

    Not using iniparser as it's not Win32 VC++ compliant

--- src/ofxIniFile.h
+++ src/ofxIniFile.h
@@ -4,27 +4,36 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include "SimpleIni.h"
+/*
 extern "C" {
 	#include "iniparser.h"
 }
+*/
 
 /**
  * Simple wrapper for a c-style minimal ini setting parser.
  */
 class ofxIniFile {
 public:
-	ofxIniFile(char* sFile);
+	ofxIniFile(std::string  sFile);
 	~ofxIniFile();
 
-	int	get(const char* sKey, int defaultValue);
-	double get(const char* sKey, double defaultValue);
-	char* get(const char* sKey, char* sDefaultValue);
-	bool get(const char* sKey, bool defaultValue);
-	std::vector<std::string> getStringVector(const char* sKey, char* sDefaultValue, char sSeparator = ',');
+	long            getLong(std::string sSection, std::string  sKey, long           nDefaultValue);
+	int             getInt(std::string sSection, std::string  sKey, int            nDefaultValue);
+    std::string     getString(std::string sSection, std::string  sKey, std::string    sDefaultValue);
+    bool            getBool(std::string sSection, std::string  sKey, bool           bDefaultValue = false);
+    std::vector<std::string> getStringVector(std::string sSection, std::string  sKey, std::string  sDefaultValue, char sSeparator = ',');
+/*
 
+
+
+	std::vector<std::string> getStringVector(std::string sSection, std::string  sKey, std::string  sDefaultValue, char sSeparator = ',');
+*/
 private:
-	const char* ini_file;
-	dictionary*  ini;
+    CSimpleIniA  ini;
+	std::string  ini_file;
+	//dictionary*  ini;
 };
 
 #endif
